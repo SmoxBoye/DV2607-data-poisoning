eb3107d93d010f2a0ff635bfa37643aa9b1ab406
SmoxBoye <33005387+SmoxBoye@users.noreply.github.com>
2022-11-02 21:02:24 +0100

    First working attack

--- Part1/client.py
+++ Part1/client.py
@@ -6,6 +6,7 @@ import json
 import pandas as pd
 import requests
 
+
 UID = 2607
 COMMANDS = ["-a", "-e", "-h", "-m", "-r", "-t"]
 example_input = [
@@ -31,14 +32,58 @@ example_input = [
         "PAY_AMT4": 18,
         "PAY_AMT5": 19,
         "PAY_AMT6": 20,
-        "default payment next month": 0
-    }
+        "default payment next month": 0,
+    },
 ]
 multiple_example_inputs = [
     {"UID": UID},
-    {"LIMIT_BAL": 1,"AGE": 2,"PAY_0": 3,"PAY_2": 4,"PAY_3": 5,"PAY_4": 6,"PAY_5": 7,"PAY_6": 8,"BILL_AMT1": 9,"BILL_AMT2": 10,"BILL_AMT3": 11,"BILL_AMT4": 12,"BILL_AMT5": 13,"BILL_AMT6": 14,"PAY_AMT1": 15,"PAY_AMT2": 16,"PAY_AMT3": 17,"PAY_AMT4": 18,"PAY_AMT5": 19,"PAY_AMT6": 20,"default payment next month": 0},
-    {"LIMIT_BAL": 1,"AGE": 2,"PAY_0": 3,"PAY_2": 4,"PAY_3": 5,"PAY_4": 6,"PAY_5": 7,"PAY_6": 8,"BILL_AMT1": 9,"BILL_AMT2": 10,"BILL_AMT3": 11,"BILL_AMT4": 12,"BILL_AMT5": 13,"BILL_AMT6": 14,"PAY_AMT1": 15,"PAY_AMT2": 16,"PAY_AMT3": 17,"PAY_AMT4": 18,"PAY_AMT5": 19,"PAY_AMT6": 20,"default payment next month": 1}
-]    
+    {
+        "LIMIT_BAL": 1,
+        "AGE": 2,
+        "PAY_0": 3,
+        "PAY_2": 4,
+        "PAY_3": 5,
+        "PAY_4": 6,
+        "PAY_5": 7,
+        "PAY_6": 8,
+        "BILL_AMT1": 9,
+        "BILL_AMT2": 10,
+        "BILL_AMT3": 11,
+        "BILL_AMT4": 12,
+        "BILL_AMT5": 13,
+        "BILL_AMT6": 14,
+        "PAY_AMT1": 15,
+        "PAY_AMT2": 16,
+        "PAY_AMT3": 17,
+        "PAY_AMT4": 18,
+        "PAY_AMT5": 19,
+        "PAY_AMT6": 20,
+        "default payment next month": 0,
+    },
+    {
+        "LIMIT_BAL": 1,
+        "AGE": 2,
+        "PAY_0": 3,
+        "PAY_2": 4,
+        "PAY_3": 5,
+        "PAY_4": 6,
+        "PAY_5": 7,
+        "PAY_6": 8,
+        "BILL_AMT1": 9,
+        "BILL_AMT2": 10,
+        "BILL_AMT3": 11,
+        "BILL_AMT4": 12,
+        "BILL_AMT5": 13,
+        "BILL_AMT6": 14,
+        "PAY_AMT1": 15,
+        "PAY_AMT2": 16,
+        "PAY_AMT3": 17,
+        "PAY_AMT4": 18,
+        "PAY_AMT5": 19,
+        "PAY_AMT6": 20,
+        "default payment next month": 1,
+    },
+]
 
 PATHS = {
     "original_data": "data/data.csv",
@@ -46,27 +91,40 @@ PATHS = {
     "test_data": "data/test_data.csv",
     "original_model": "models/rfc_original.pkl",
     "latest_model": "models/rfc_latest.pkl",
-    "model_metadata": "models/model_metadata.ini"
+    "model_metadata": "models/model_metadata.ini",
 }
 
+
 def launch_attack():  # Implement this
-    """ python client.py -a
+    """python client.py -a
     Launches a data poisoning attack on the server by feeding it with
-    incorrectly labeled data """
+    incorrectly labeled data"""
 
     # Read legitimate data
     train_data = pd.read_csv(PATHS["train_data"])
 
-    # Example of code that changes the first feature in the data to integer 
+    # Example of code that changes the first feature in the data to integer
     # value 42 looks as follows: train_data["LIMIT_BAL"] = 42
     # Add your own attack code below.
-    
+
     # <START ATTACK CODE>
-    
-    # Add your poisoning attack code here ...
+
+    # APPLY LABEL FLIP ATTACK tp cancel out original data --> Expected AUC = 50%
+    train_data["default payment next month"] = train_data[
+        "default payment next month"
+    ].apply(lambda x: 1 if x == 0 else 0)
+
+    train_data = pd.concat([train_data, train_data])
+    # poison_data = dict(
+    #    zip(train_data.columns, [range(10000) for _ in train_data.columns[:-1]])
+    # )
+
+    # poison_data["default payment next month"] = [0 for _ in range(10000)]
+    # poison_dataframe = pd.DataFrame(poison_data)
+    # train_data = pd.concat([train_data, poison_dataframe])
+    print(train_data)
 
     # <END ATTACK CODE, leave code below unchanged>
-    
 
     payload = train_data.to_dict("records")
     payload.insert(0, {"UID": UID})
@@ -78,16 +136,16 @@ def launch_attack():  # Implement this
 
 @timeit
 def submit_data(inp):
-    """ python client.py -d
+    """python client.py -d
     Requests the server to store the submitted data for training of the
     server's ML-based classification system
 
-    The input (inp) must be a list of dictionaries containing all the keys 
-    and with all dict values being integers, see example entry above. The 
-    class attribute 'default payment next month' must be either 0 or 1. 
+    The input (inp) must be a list of dictionaries containing all the keys
+    and with all dict values being integers, see example entry above. The
+    class attribute 'default payment next month' must be either 0 or 1.
     The first element of the list must be a dict with the single key 'UID'.
 
-    For bulk submission, just append more dictionaries to the list. """
+    For bulk submission, just append more dictionaries to the list."""
 
     r = requests.post("http://localhost:5000/add", json=inp)
 
@@ -100,9 +158,9 @@ def submit_data(inp):
 
 
 def get_model_report():
-    """ python client.py -m
+    """python client.py -m
     Requests a classification performance report of the trained model on the
-    server and prints the results """
+    server and prints the results"""
 
     r = requests.get("http://localhost:5000/report")
 
@@ -123,29 +181,35 @@ def get_model_report():
 
 
 def print_usage():
-    """ python client.py -h
-    Prints the available commands for the client application """
+    """python client.py -h
+    Prints the available commands for the client application"""
 
     print("Available commands:")
     print("   -a      Launch a data poisoning attack on the server.")
-    print("   -e      Example of new data submission for storage in the "
-          "server's dataset.")
+    print(
+        "   -e      Example of new data submission for storage in the "
+        "server's dataset."
+    )
     print("   -h      Print this help message.")
-    print("   -m      Retrieve a classification performance report of the "
-          "server's trained model.")
-    print("   -r      Reset the state of the machine learning model and the "
-          "stored data.")
+    print(
+        "   -m      Retrieve a classification performance report of the "
+        "server's trained model."
+    )
+    print(
+        "   -r      Reset the state of the machine learning model and the "
+        "stored data."
+    )
     print("   -t      Start training the model on all collected data.")
 
 
 @timeit
 def reset_state():
-    """ python client.py -r
-    Resets the machine learning model and it's stored data on the server. """
-    
-    #answer = input("Are you sure you want to reset the server's machine "
+    """python client.py -r
+    Resets the machine learning model and it's stored data on the server."""
+
+    # answer = input("Are you sure you want to reset the server's machine "
     #               "learning model and it's stored data? [y/N] ")
-    #if answer != 'y':
+    # if answer != 'y':
     #    print("Aborting!")
     #    return
 
@@ -159,8 +223,8 @@ def reset_state():
 
 @timeit
 def train_model():
-    """ python client.py -t
-    Requests the server to train the model with the available training data """
+    """python client.py -t
+    Requests the server to train the model with the available training data"""
 
     print("Training model, please wait...")
     r = requests.post("http://localhost:5000/train")
@@ -174,16 +238,16 @@ def train_model():
 if len(sys.argv) < 2 or sys.argv[1] not in COMMANDS:
     print_usage()
     exit()
-elif sys.argv[1] == '-a':
+elif sys.argv[1] == "-a":
     launch_attack()
-elif sys.argv[1] == '-e':
-    #submit_data(example_input)
+elif sys.argv[1] == "-e":
+    # submit_data(example_input)
     submit_data(multiple_example_inputs)
-elif sys.argv[1] == '-h':
+elif sys.argv[1] == "-h":
     print_usage()
-elif sys.argv[1] == '-m':
+elif sys.argv[1] == "-m":
     get_model_report()
-elif sys.argv[1] == '-r':
+elif sys.argv[1] == "-r":
     reset_state()
-elif sys.argv[1] == '-t':
+elif sys.argv[1] == "-t":
     train_model()
